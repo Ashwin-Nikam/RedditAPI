80283d282726024d29794a131ad48c5bc9d8610a
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-04-26 15:55:27 -0400

    Typed a simple code

--- Main.py
+++ Main.py
@@ -1,5 +1,20 @@
+import json
+
 import requests
 import bs4
-from bs4 import BeautifulSoup as bs
 
-index_url = ""
+
+def get_news():
+    index_url = "http://www.reddit.com/"
+    response = requests.get(index_url)
+    ret = []
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    posts = soup.select("div#siteTable div.thing")
+    for post in posts:
+        info = post.select("p.title a")[0]
+        subreddit = post.select("p.tagline a")[1].text.capitalize()
+        ret.append({"title":info.text, "link":info["href"], "subreddit": subreddit})
+    return json.dumps(ret)
+
+
+print(get_news())
