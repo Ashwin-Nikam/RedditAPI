1f1a7afe8dca151eb2591d841a01cd217030dc08
Ashwin-Nikam <nikam.ashwin7@gmail.com>
2018-04-26 16:13:12 -0400

    Successfully querying the reddit api

--- Main.py
+++ Main.py
@@ -1,20 +1,27 @@
 import json
-
 import requests
 import bs4
 
 
 def get_news():
-    index_url = "http://www.reddit.com/"
-    response = requests.get(index_url)
+    index_url = "http://www.reddit.com/top/"
+    session = requests.Session()
+    session.headers.update({'User-Agent': 'Custom user agent'})
+    response = session.get(index_url)
+    print(response.text)
     ret = []
     soup = bs4.BeautifulSoup(response.text, "html.parser")
     posts = soup.select("div#siteTable div.thing")
     for post in posts:
+        dict = {}
         info = post.select("p.title a")[0]
         subreddit = post.select("p.tagline a")[1].text.capitalize()
-        ret.append({"title":info.text, "link":info["href"], "subreddit": subreddit})
-    return json.dumps(ret)
+        dict['title'] = info.text
+        dict['link'] = info['href']
+        dict['subreddit'] = subreddit
+        ret.append(dict)
+    return ret
 
 
-print(get_news())
+posts = get_news()
+print("hello")
